315e84b49561c2d172d2df941d2f3a54e7a5505b
Czebosak <czebosak@proton.me>
2025-04-30 20:02:22 +0200

    Added proper callback removing

--- src/component.hpp
+++ src/component.hpp
@@ -1,15 +1,16 @@
 #pragma once
-#include <string_name.hpp>
+#include <unordered_set>
 #include <typeinfo>
+
+#include <string_name.hpp>
+#include <event.hpp>
 
 class Entity;
 
 class Component {
 protected:
     mutable std::optional<StringName> name;
-public: 
-    virtual ~Component() = default;
-
+public:
     const StringName& get_name() const {
         if (!name.has_value()) {
             name = std::make_optional(StringName(typeid(*this).name()));
--- src/event.hpp
+++ src/event.hpp
@@ -4,7 +4,7 @@
 
 #include <string_name.hpp>
 
-#include <component.hpp>
+class Component;
 
 #define BIND_EVENT_CALLBACK(callback) this, [this](auto&&... args) { this->callback(std::forward<decltype(args)>(args)...); }
 
@@ -15,12 +15,23 @@ public:
 private:
     std::unordered_multimap<Component*, Callback> listeners;
 public:
-
     void subscribe(Component* listener, const Callback& callback) {
         listeners.emplace(listener, callback);
     }
 
-    void unsubscribe(Component* listener) {
+    void unsubscribe(Component* listener, const Callback& callback) {
+        // Traverse the values to find the right callback
+        auto range = listeners.equal_range(listener);
+        for (auto it = range.first; it != range.second; ++it) {
+            auto& current_callback = it->second;
+            if (callback == current_callback) {
+                listeners.erase(it);
+                break;
+            }
+        }
+    }
+
+    void unsubscribe_component(Component* listener) {
         listeners.erase(listener);
     }
 
